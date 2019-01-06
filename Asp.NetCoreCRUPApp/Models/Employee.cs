d3c6709369aa37e17d3376e958026127113a0a45
rajib kumar bhowmik <rajib.bhowmik@gmail.com>
2019-01-06 22:31:13 +0100

    Add project files.

---
+++ Asp.NetCoreCRUPApp/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreCRUPApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name ="Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [Display(Name = "Employee Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [Display(Name = "Office Location")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
